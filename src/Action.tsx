6823895d323a911adf8db6a3cf96ad00c2e28046
williamvasiliou <195391054+williamvasiliou@users.noreply.github.com>
2025-01-21 23:57:13 +0000

    Create Action.tsx

---
+++ src/Action.tsx
@@ -0,0 +1,17 @@
+export const ACTIONS = [
+	'Viewing',
+	'Editing',
+	'Deleting',
+	'Splitting',
+	'Joining',
+	'Moving',
+	'Searching',
+]
+
+export const VIEWING = 0
+export const EDITING = 1
+export const DELETING = 2
+export const SPLITTING = 3
+export const JOINING = 4
+export const MOVING = 5
+export const SEARCHING = 6
