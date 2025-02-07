a83eff79e95a839b9b2170ff40de1392c978d794
williamvasiliou <195391054+williamvasiliou@users.noreply.github.com>
2025-02-07 23:54:40 +0000

    Add files via upload

--- src/components/AnalysisJobs.tsx
+++ src/components/AnalysisJobs.tsx
@@ -3,10 +3,17 @@ import AnalysisLabels from './AnalysisLabels'
 function AnalysisJobs({ jobs, currentAnalysis, setCurrentAnalysis }) {
 	return (
 		<ol>
-			{jobs.map(({ id, job, labels }) => (
+			{jobs.map(({ id, title, url, labels }) => (
 				<li key={id}>
 					<hr/>
-					<h4>{job}</h4>
+					<h4>{title}</h4>
+					{url ? (
+						<a href={url} target='_blank'>{url}</a>
+					) : (
+						<em>URL</em>
+					)}
+					<br/>
+					<br/>
 					<AnalysisLabels
 						labels={labels}
 						jobId={id}
